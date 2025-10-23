f0c5ad807077755fe1e142b25039b41e86e313e6
benny_neiman <bn161299@gmail.com>
2025-10-23 12:25:25 +0300

    update

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,3 +1,25 @@
-create_player(name:str) -> dict
-init_game()->dict
-play_round(p1:dict,p2:dict)
+def create_player(name:str="ai"):
+    hend=[]
+    won_pile=[]
+    player_info={"name":name,"hend":hend,"won_pile":won_pile}
+    return player_info
+
+
+
+
+
+
+
+def init_game():
+    
+
+    return create_player()
+        def init-game2():
+        
+    
+    
+    
+    
+
+print(init_game())
+# play_round(p1:dict,p2:dict)
--- utils/deck.py
+++ utils/deck.py
@@ -1,4 +1,61 @@
-create_card(rank:str,suite:str) -> dict
- compare_cards(p1_card:dict, p2_card:dict) -> str
-create_deck() -> list[dict]
-shuffle(deck:list[dict]) -> list[dict]
+def create_card(rank:str,suite:str):
+    vel_num=["2","3","4","5","6","7","8","9","10"]
+    vel_letaer={"J":11,"Q":12,"K":13,"A":14}
+    card_dict={"rank":rank,"suite":suite}
+    if rank in vel_num:
+        card_dict["velue"]=(rank)
+    elif rank in vel_letaer:
+        card_dict["velue"]=(vel_letaer[rank])
+    return card_dict
+
+# def compare_card(p1_card:dict, p2_card:dict):
+#     if p1_card["velue"] > p2_card['velue']:
+#         return'p1'
+#     elif p1_card["velue"] < p2_card['velue']:
+#         return 'p2'
+#     elif p1_card["velue"] == p2_card['velue']:
+#         return "WAR"
+
+
+def create_deck():
+    vel_num=["2","3","4","5","6","7","8","9","10"]
+    vel_letaer={"J":11,"Q":12,"K":13,"A":14}
+    suits_list=["H",'C','D','S']
+    list_of_cards=[]
+
+    for num in vel_num:
+        for l in suits_list:
+            list_of_cards.append(create_card(num,l))
+    for vl in vel_letaer:
+        for l in suits_list:
+            list_of_cards.append(create_card(vl,l))
+    return list_of_cards
+    
+
+
+
+
+
+list_of_cards=create_deck()
+    
+
+
+
+
+def shuffle(deck:list[dict]):
+    list_of_cards=create_deck()
+    shuffle_list=[]
+
+
+
+    import random
+    for i in range(random.randrange(1,1001)):
+        list_of_cards[random.randint(1,51)]=list_of_cards[random.randint(1,51)]
+        
+        shuffle_list=list_of_cards
+        
+        
+                
+    return shuffle_list
+
+
