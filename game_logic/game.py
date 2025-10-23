d4193851d5f01f56d346f7a3827a8a072df4b4c3
benny_neiman <bn161299@gmail.com>
2025-10-23 09:18:50 +0300

    first commit

---
+++ game_logic/game.py
@@ -0,0 +1,3 @@
+create_player(name:str) -> dict
+init_game()->dict
+play_round(p1:dict,p2:dict)
---
+++ utils/deck.py
@@ -0,0 +1,4 @@
+create_card(rank:str,suite:str) -> dict
+ compare_cards(p1_card:dict, p2_card:dict) -> str
+create_deck() -> list[dict]
+shuffle(deck:list[dict]) -> list[dict]
